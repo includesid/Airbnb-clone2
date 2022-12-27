e2abbff77a12f910865236a5b8c3fe9440a94cf7
includesid <depanshuiit2017@gmail.com>
2022-12-27 16:28:35 +0530

    commit1

---
+++ build/precache-manifest.c729e1bd36a39887ec68d53174684860.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efbd49a58f10b68ff2b035e8806e395",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/static/css/2.a1840750.chunk.css"
+  },
+  {
+    "revision": "c4532fc8e966aa353909",
+    "url": "/static/css/main.6777df22.chunk.css"
+  },
+  {
+    "revision": "5204efd19cd4bb25612c",
+    "url": "/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4532fc8e966aa353909",
+    "url": "/static/js/main.57e26ca4.chunk.js"
+  },
+  {
+    "revision": "9d9a4c99211d98368a0d",
+    "url": "/static/js/runtime-main.7b3982eb.js"
+  }
+]);
